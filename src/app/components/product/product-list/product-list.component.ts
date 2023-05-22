9d33a984c6be14d1c77eb9d34260e8af9b6a2636
SithuKyaw-Thitsaworks <sithu.kyaw@thitsaworks.com>
2023-05-23 01:49:27 +0630

    Added search function to product list page

--- src/app/components/product/product-list/product-list.component.ts
+++ src/app/components/product/product-list/product-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../services/product.service';
 import { IProduct } from '../../../models/product';
+import { HttpClient } from '@angular/common/http';
+import { ICategory } from 'src/app/models/category';
+import { IBrand } from 'src/app/models/brand';
+import { CategoryService } from 'src/app/services/category.service';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,12 +13,57 @@ import { IProduct } from '../../../models/product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  productName: string = "";
+  categoryId: string = "";
+  brandId: string = "";
   products: IProduct[] = [];
+  categories: ICategory[] = [];
+  brands: IBrand[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private brandService: BrandService) { }
 
   ngOnInit(): void {
     this.getProducts();
+    this.loadCategories();
+    this.loadBrands();
+  }
+
+
+  loadCategories(): void {
+    this.categoryService.getCategories().subscribe(
+      (categories) => {
+        this.categories = categories;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  loadBrands(): void {
+    this.brandService.getBrands().subscribe(
+      (brands) => {
+        this.brands = brands;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  searchProducts(): void {
+    this.productService.searchProducts(this.productName, this.brandId, this.categoryId)
+      .subscribe(
+        (response) => {
+          this.products = response;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   }
 
   getProducts(): void {
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -20,6 +20,23 @@ export class ProductService {
     return this.http.get<IProduct>(url);
   }
 
+  searchProducts(name?: string, brandId?: string, categoryId?: string): Observable<any[]> {
+    let params = '';
+    if (name) {
+      params += `name=${encodeURIComponent(name)}&`;
+    }
+    if (brandId) {
+      params += `brandId=${encodeURIComponent(brandId)}&`;
+    }
+    if (categoryId) {
+      params += `categoryId=${encodeURIComponent(categoryId)}&`;
+    }
+    params = params.slice(0, -1); // Remove trailing "&" character
+
+    const url = `${this.apiUrl}/filter?${params}`;
+    return this.http.get<any[]>(url);
+  }
+
   createProduct(product: IProduct): Observable<IProduct> {
     return this.http.post<IProduct>(this.apiUrl, product);
   }
